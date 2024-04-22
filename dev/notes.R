23e53df0772e77809a3ae697c69cf4fc1cfcf7a7
Dudel <dudel@demogr.mpg.de>
2024-04-22 14:07:44 +0200

    Forgot to mention with last commit: also changed dtms_formula; this commit: edited notes

--- dev/notes.R
+++ dev/notes.R
@@ -1,27 +1,5 @@
-library(dtms)
-library(devtools)
-simple <- dtms(transient=c("A","B"),
-               absorbing="X",
-               timescale=0:19)
-estdata <- dtms_format(data=simpledata,
-                       dtms=simple,
-                       idvar="id",
-                       timevar="time",
-                       statevar="state")
-estdata <- dtms_clean(data=estdata,
-                      dtms=simple)
-## Fit model
-fit1 <- dtms_fit(data=estdata,package="nnet")
-fit2 <- dtms_fit(data=estdata,package="VGAM")
-fit3 <- dtms_fit(data=estdata,package="mclogit",random=~1|id)
-
-## Variance-covariance matrices
-vcov(fit1)
-vcov(fit2)
-vcov(fit3)
-
-
-##########################################
+#######################
+# Bias corrected bootstrap?
 
 install.packages("coxed")
 library(coxed)
