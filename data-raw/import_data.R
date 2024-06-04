d90de36bd15f41be4a2373669a3bb14e19509262
Dudel <dudel@demogr.mpg.de>
2024-06-04 16:36:32 +0200

    .

--- data-raw/import_data.R
+++ data-raw/import_data.R
@@ -1,13 +1,14 @@
+# Sources/code on Github: https://www.github.com/christiandudel/dtms_data
 ### Package
 library(devtools)
 
 ### Import simple example, currently only exists locally
-load("U:/Documents/__CODE__/Markov Chain Simulation/Data/simple.rda")
+load("https://www.github.com/christiandudel/dtms_data/Data/simple.rda")
 simpledata <- as.data.frame(simdata)
 usethis::use_data(simpledata,overwrite=TRUE)
 
 ### Import simulated HRS data, currently only exists locally
 # based on: https://doi.org/10.1007/s13524-017-0619-6
-load("U:/Documents/__CODE__/Markov Chain Simulation/Data/hrs.rda")
+load("https://www.github.com/christiandudel/dtms_data/Data/simple.rda")
 hrsdata <- as.data.frame(simdata)
 usethis::use_data(hrsdata,overwrite=TRUE)
