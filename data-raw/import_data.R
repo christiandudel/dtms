bfce5109c5da3b01f80f02a344d77e6f72577217
Dudel <dudel@demogr.mpg.de>
2024-06-06 10:35:53 +0200

    Updated workflow for example data

--- data-raw/import_data.R
+++ data-raw/import_data.R
@@ -3,12 +3,12 @@
 library(devtools)
 
 ### Import simple example, currently only exists locally
-load("https://www.github.com/christiandudel/dtms_data/Data/simple.rda")
+load(url("https://github.com/christiandudel/dtms_data/raw/main/Output/simple.rda"))
 simpledata <- as.data.frame(simdata)
 usethis::use_data(simpledata,overwrite=TRUE)
 
 ### Import simulated HRS data, currently only exists locally
 # based on: https://doi.org/10.1007/s13524-017-0619-6
-load("https://www.github.com/christiandudel/dtms_data/Data/simple.rda")
+load(url("https://github.com/christiandudel/dtms_data/raw/main/Output/hrs.rda"))
 hrsdata <- as.data.frame(simdata)
 usethis::use_data(hrsdata,overwrite=TRUE)
